2de41171cb9f04c07eff864a05bfab1ead87302f
yuanyxh <15766118362@139.com>
2023-03-01 21:56:50 +0800

    项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    Node: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {}
+};
